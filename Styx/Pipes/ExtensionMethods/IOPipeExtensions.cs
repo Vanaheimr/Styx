8b4c55a0b7fd206e21245ec477123c7128bc2952
Achim Friedland <github@ahzf.de>
2012-03-27 16:31:19 +0200

    Added a FixLineBreakPipe to fix new line characters in CSV files.

--- Styx/Pipes/ExtensionMethods/IOPipeExtensions.cs
+++ Styx/Pipes/ExtensionMethods/IOPipeExtensions.cs
@@ -18,9 +18,8 @@
 #region Usings
 
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Text.RegularExpressions;
+using System.Collections.Generic;
 
 #endregion
 
@@ -66,59 +65,6 @@ namespace de.ahzf.Styx
                                                                 FileFilter          FileFilter    = null)
         {
             return new FileFilterPipe(SearchPattern, SearchOption, FileFilter, null, IEnumerator);
-        }
-
-        #endregion
-
-
-        #region CSVPipe(this IEnumerable, IgnoreLines = null, Seperators = null, StringSplitOptions = None, ExpectedNumberOfColumns = null, FailOnWrongNumberOfColumns = false, TrimColumns = true)
-
-        /// <summary>
-        /// Splits a given strings into elements by a given sperator.
-        /// </summary>
-        /// <param name="IEnumerable">An enumeration of strings.</param>
-        /// <param name="IgnoreLines">A regular expression indicating which input strings should be ignored. Default: All lines starting with a '#'.</param>
-        /// <param name="Seperators">An array of string used to split the input strings.</param>
-        /// <param name="StringSplitOptions">Split options, e.g. remove empty entries.</param>
-        /// <param name="ExpectedNumberOfColumns">If the CSV file had a schema, a specific number of columns can be expected. If instead it is a list of values no such value can be expected.</param>
-        /// <param name="FailOnWrongNumberOfColumns">What to do when the current and expected number of columns do not match.</param>
-        /// <param name="TrimColumns">Remove leading and trailing whitespaces.</param>
-        /// <returns>An enumeration of string arrays.</returns>
-        public static IEnumerable<String[]> CSVPipe(this IEnumerable<String> IEnumerable,
-                                                         Regex               IgnoreLines                = null,
-                                                         String[]            Seperators                 = null,
-                                                         StringSplitOptions  StringSplitOptions         = StringSplitOptions.None,
-                                                         UInt16?             ExpectedNumberOfColumns    = null,
-                                                         Boolean             FailOnWrongNumberOfColumns = false,
-                                                         Boolean             TrimColumns                = true)
-        {
-            return new CSVReaderPipe(IgnoreLines, Seperators, StringSplitOptions, ExpectedNumberOfColumns, FailOnWrongNumberOfColumns, TrimColumns, IEnumerable, null);
-        }
-
-        #endregion
-
-        #region CSVPipe(this IEnumerator, IgnoreLines = null, Seperators = null, StringSplitOptions = None, ExpectedNumberOfColumns = null, FailOnWrongNumberOfColumns = false, TrimColumns = true)
-
-        /// <summary>
-        /// Splits a given strings into elements by a given sperator.
-        /// </summary>
-        /// <param name="IEnumerator">An enumerator of strings.</param>
-        /// <param name="IgnoreLines">A regular expression indicating which input strings should be ignored. Default: All lines starting with a '#'.</param>
-        /// <param name="Seperators">An array of string used to split the input strings.</param>
-        /// <param name="StringSplitOptions">Split options, e.g. remove empty entries.</param>
-        /// <param name="ExpectedNumberOfColumns">If the CSV file had a schema, a specific number of columns can be expected. If instead it is a list of values no such value can be expected.</param>
-        /// <param name="FailOnWrongNumberOfColumns">What to do when the current and expected number of columns do not match.</param>
-        /// <param name="TrimColumns">Remove leading and trailing whitespaces.</param>
-        /// <returns>An enumeration of string arrays.</returns>
-        public static IEnumerable<String[]> CSVPipe(this IEnumerator<String> IEnumerator,
-                                                         Regex               IgnoreLines                = null,
-                                                         String[]            Seperators                 = null,
-                                                         StringSplitOptions  StringSplitOptions         = StringSplitOptions.None,
-                                                         UInt16?             ExpectedNumberOfColumns    = null,
-                                                         Boolean             FailOnWrongNumberOfColumns = false,
-                                                         Boolean             TrimColumns                = true)
-        {
-            return new CSVReaderPipe(IgnoreLines, Seperators, StringSplitOptions, ExpectedNumberOfColumns, FailOnWrongNumberOfColumns, TrimColumns, null, IEnumerator);
         }
 
         #endregion
