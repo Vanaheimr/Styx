c49c99f68760a685e68e3e86bf3492a9789dc06f
Achim Friedland <github@ahzf.de>
2013-03-02 03:45:07 +0100

    minor important stuff

---
+++ StyxTests/FromBalder/FilterPipes/AndFilterPipeTest.cs
@@ -0,0 +1,80 @@
+/*
+ * Copyright (c) 2010-2013, Achim 'ahzf' Friedland <achim@graph-database.org>
+ * This file is part of Styx <http://www.github.com/Vanaheimr/Styx>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#region Usings
+
+using System;
+using System.Collections.Generic;
+
+using NUnit.Framework;
+
+#endregion
+
+namespace de.ahzf.Vanaheimr.Styx.UnitTests.FilterPipes
+{
+
+    [TestFixture]
+    public class AndFilterPipeTest
+    {
+
+        #region testAndPipeBasic()
+
+        [Test]
+        public void testAndPipeBasic()
+        {
+
+            var _Names = new List<String>() { "marko", "povel", "peter", "povel", "marko" };
+            var _Pipe1 = new ObjectFilterPipe<String>("marko", ComparisonFilter.NOT_EQUAL);
+            var _Pipe2 = new ObjectFilterPipe<String>("povel", ComparisonFilter.NOT_EQUAL);
+            var _AndFilterPipe = new AndFilterPipe<String>(new HasNextPipe<String>(_Pipe1), new HasNextPipe<String>(_Pipe2));
+            _AndFilterPipe.SetSourceCollection(_Names);
+
+            var _Counter = 0;
+            while (_AndFilterPipe.MoveNext())
+                _Counter++;
+
+            Assert.AreEqual(0, _Counter);
+
+        }
+
+        #endregion
+
+        #region testAndPipeBasic2()
+
+        [Test]
+        public void testAndPipeBasic2()
+        {
+
+            var _Names          = new List<String>() { "marko", "povel", "peter", "povel", "marko" };
+            var _Pipe1          = new ObjectFilterPipe<String>("marko", ComparisonFilter.NOT_EQUAL);
+            var _Pipe2          = new ObjectFilterPipe<String>("marko", ComparisonFilter.NOT_EQUAL);
+            var _AndFilterPipe  = new AndFilterPipe<String>(new HasNextPipe<String>(_Pipe1), new HasNextPipe<String>(_Pipe2));
+            _AndFilterPipe.SetSourceCollection(_Names);
+
+            var _Counter = 0;
+            while (_AndFilterPipe.MoveNext())
+                _Counter++;
+
+            Assert.AreEqual(2, _Counter);
+
+        }
+
+        #endregion
+
+    }
+
+}
--- StyxTests/FromBalder/FilterPipes/FutureFilterPipeTest.cs
+++ StyxTests/FromBalder/FilterPipes/FutureFilterPipeTest.cs
@@ -1,4 +1,4 @@
-﻿/*
+/*
  * Copyright (c) 2010-2013, Achim 'ahzf' Friedland <achim@graph-database.org>
  * This file is part of Styx <http://www.github.com/Vanaheimr/Styx>
  *
@@ -18,61 +18,60 @@
 #region Usings
 
 using System;
-using System.Linq;
 using System.Collections.Generic;
 
 using NUnit.Framework;
 
 #endregion
 
-namespace de.ahzf.Vanaheimr.Styx.UnitTests.util
+namespace de.ahzf.Vanaheimr.Styx.UnitTests.FilterPipes
 {
 
     [TestFixture]
-    public class ScatterPipeTests
+    public class FutureFilterPipeTest
     {
 
-        #region testScatterPipe()
+        #region testBasicFutureFilter()
 
         [Test]
-        public void testScatterPipe()
+        public void testBasicFutureFilter()
         {
 
-            var _Scatter = new ScatterPipe<Int32, Int32>();
-            _Scatter.SetSourceCollection(new List<Int32>() { 1, 2, 3 });
+            var _Names	= new List<String>() { "marko", "povel", "peter", "josh" };
+            var _Pipe1	= new FutureFilterPipe<String>(new IdentityPipe<String>());
+            _Pipe1.SetSourceCollection(_Names);
 
             var _Counter = 0;
-            while (_Scatter.MoveNext())
+            while (_Pipe1.MoveNext())
             {
-                var _Object = _Scatter.Current;
-                Assert.IsTrue(_Object.Equals(1) || _Object.Equals(2) || _Object.Equals(3));
                 _Counter++;
             }
 
-            Assert.AreEqual(3, _Counter);
+            Assert.AreEqual(4, _Counter);
 
         }
 
         #endregion
 
-        #region testScatterPipeComplex()
+        #region testAdvancedFutureFilter()
 
         [Test]
-        public void testScatterPipeComplex()
+        public void testAdvancedFutureFilter()
         {
 
-            var _Scatter = new ScatterPipe<Object, Int32>();
-            _Scatter.SetSourceCollection(new List<Object>() { 1, 2, new List<Object>() { 3, 4 }, 5, 6 });
+            var _Names = new List<String>() { "marko", "povel", "peter", "josh" };
+            var _Pipe  = new FutureFilterPipe<String>(new CollectionFilterPipe<String>(new List<String>() { "marko", "povel" }, ComparisonFilter.EQUAL));
+            _Pipe.SetSourceCollection(_Names);
 
             var _Counter = 0;
-            while (_Scatter.MoveNext())
+            while (_Pipe.MoveNext())
             {
-                var _Object = _Scatter.Current;
-                Assert.IsTrue(_Object.Equals(1) || _Object.Equals(2) || _Object.Equals(3) || _Object.Equals(4) || _Object.Equals(5) || _Object.Equals(6));
                 _Counter++;
+                var _Name = _Pipe.Current;
+                Assert.IsTrue(_Name.Equals("peter") || _Name.Equals("josh"));
             }
 
-            Assert.AreEqual(6, _Counter);
+            Assert.AreEqual(2, _Counter);
 
         }
 
