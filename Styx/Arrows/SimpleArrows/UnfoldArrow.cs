30e6a75e24d57ff134f4e5987d36e0cb6a647a32
Achim Friedland <github@ahzf.de>
2014-10-22 17:08:03 +0200

    fix

--- Styx/Arrows/SimpleArrows/UnfoldArrow.cs
+++ Styx/Arrows/SimpleArrows/UnfoldArrow.cs
@@ -1,6 +1,6 @@
 ﻿/*
  * Copyright (c) 2010-2014, Achim 'ahzf' Friedland <achim@graphdefined.org>
- * This file is part of Vanaheimr Hermod <http://www.github.com/Vanaheimr/Hermod>
+ * This file is part of Vanaheimr Styx <http://www.github.com/Vanaheimr/Styx>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
