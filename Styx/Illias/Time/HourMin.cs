b18803731937e0c0dcebafacd5890fc4a71605a4
Achim Friedland <github@ahzf.de>
2018-02-03 14:19:43 +0100

    OpeingTimes added

---
+++ Styx/Illias/Time/HourMin.cs
@@ -0,0 +1,370 @@
+﻿/*
+ * Copyright (c) 2010-2018 Achim 'ahzf' Friedland <achim.friedland@graphdefined.com>
+ * This file is part of Illias <http://www.github.com/Vanaheimr/Illias>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#region Usings
+
+using System;
+
+using org.GraphDefined.Vanaheimr.Illias;
+
+#endregion
+
+namespace org.GraphDefined.Vanaheimr.Illias
+{
+
+    /// <summary>
+    /// This class references business details of Charging Station Operators.
+    /// </summary>
+    public struct HourMin : IEquatable<HourMin>, IComparable<HourMin>, IComparable
+    {
+
+        #region Properties
+
+        /// <summary>
+        /// The hour.
+        /// </summary>
+        public Byte Hour     { get; }
+
+        /// <summary>
+        /// The minute.
+        /// </summary>
+        public Byte Minute   { get; }
+
+        #endregion
+
+        #region Constructor(s)
+
+        /// <summary>
+        /// Create new hour/minute.
+        /// </summary>
+        /// <param name="Hour">The hour.</param>
+        /// <param name="Minute">The minute.</param>
+        public HourMin(Byte  Hour,
+                       Byte  Minute)
+        {
+
+            #region Initial checks
+
+            if (Hour > 23)
+                throw new ArgumentException("The given hour is invalid!",   nameof(Hour));
+
+            if (Minute > 59)
+                throw new ArgumentException("The given minute is invalid!", nameof(Minute));
+
+            #endregion
+
+            this.Hour    = Hour;
+            this.Minute  = Minute;
+
+        }
+
+        #endregion
+
+
+        #region (static) Parse(Text)
+
+        public static HourMin Parse(String Text)
+        {
+
+            if (Text.IsNullOrEmpty())
+                throw new ArgumentNullException(nameof(Text),  "The given text must not be null or empty!");
+
+            var Splited = Text.Split(':');
+
+            if (Splited.Length != 2)
+                throw new ArgumentException("The given input '" + Text + "' is not valid!");
+
+            return new HourMin(Byte.Parse(Splited[0]),
+                               Byte.Parse(Splited[1]));
+
+        }
+
+        #endregion
+
+        #region (static) TryParse(Text, out HourMin)
+
+        public static Boolean TryParse(String Text, out HourMin HourMin)
+        {
+
+            HourMin = new HourMin(0, 0);
+
+            if (Text.IsNullOrEmpty())
+                return false;
+
+            var Splited = Text.Split(':');
+
+            if (Splited.Length != 2)
+                return false;
+
+            Byte Hour = 0;
+            if (!Byte.TryParse(Splited[0], out Hour))
+                return false;
+
+            Byte Minute = 0;
+            if (!Byte.TryParse(Splited[1], out Minute))
+                return false;
+
+            HourMin = new HourMin(Hour, Minute);
+
+            return true;
+
+        }
+
+        #endregion
+
+
+        #region Operator overloading
+
+        #region Operator == (HourMin1, HourMin2)
+
+        /// <summary>
+        /// Compares two instances of this object.
+        /// </summary>
+        /// <param name="HourMin1">A HourMin.</param>
+        /// <param name="HourMin2">Another HourMin.</param>
+        /// <returns>true|false</returns>
+        public static Boolean operator ==(HourMin HourMin1, HourMin HourMin2)
+        {
+
+            // If both are null, or both are same instance, return true.
+            if (Object.ReferenceEquals(HourMin1, HourMin2))
+                return true;
+
+            // If one is null, but not both, return false.
+            if (((Object) HourMin1 == null) || ((Object) HourMin2 == null))
+                return false;
+
+            return HourMin1.Equals(HourMin2);
+
+        }
+
+        #endregion
+
+        #region Operator != (HourMin1, HourMin2)
+
+        /// <summary>
+        /// Compares two instances of this object.
+        /// </summary>
+        /// <param name="HourMin1">A HourMin.</param>
+        /// <param name="HourMin2">Another HourMin.</param>
+        /// <returns>true|false</returns>
+        public static Boolean operator !=(HourMin HourMin1, HourMin HourMin2)
+        {
+            return !(HourMin1 == HourMin2);
+        }
+
+        #endregion
+
+        #region Operator <  (HourMin1, HourMin2)
+
+        /// <summary>
+        /// Compares two instances of this object.
+        /// </summary>
+        /// <param name="HourMin1">A HourMin.</param>
+        /// <param name="HourMin2">Another HourMin.</param>
+        /// <returns>true|false</returns>
+        public static Boolean operator <(HourMin HourMin1, HourMin HourMin2)
+        {
+
+            if ((Object) HourMin1 == null)
+                throw new ArgumentNullException("The given HourMin1 must not be null!");
+
+            return HourMin1.CompareTo(HourMin2) < 0;
+
+        }
+
+        #endregion
+
+        #region Operator <= (HourMin1, HourMin2)
+
+        /// <summary>
+        /// Compares two instances of this object.
+        /// </summary>
+        /// <param name="HourMin1">A HourMin.</param>
+        /// <param name="HourMin2">Another HourMin.</param>
+        /// <returns>true|false</returns>
+        public static Boolean operator <=(HourMin HourMin1, HourMin HourMin2)
+        {
+            return !(HourMin1 > HourMin2);
+        }
+
+        #endregion
+
+        #region Operator >  (HourMin1, HourMin2)
+
+        /// <summary>
+        /// Compares two instances of this object.
+        /// </summary>
+        /// <param name="HourMin1">A HourMin.</param>
+        /// <param name="HourMin2">Another HourMin.</param>
+        /// <returns>true|false</returns>
+        public static Boolean operator >(HourMin HourMin1, HourMin HourMin2)
+        {
+
+            if ((Object) HourMin1 == null)
+                throw new ArgumentNullException("The given HourMin1 must not be null!");
+
+            return HourMin1.CompareTo(HourMin2) > 0;
+
+        }
+
+        #endregion
+
+        #region Operator >= (HourMin1, HourMin2)
+
+        /// <summary>
+        /// Compares two instances of this object.
+        /// </summary>
+        /// <param name="HourMin1">A HourMin.</param>
+        /// <param name="HourMin2">Another HourMin.</param>
+        /// <returns>true|false</returns>
+        public static Boolean operator >=(HourMin HourMin1, HourMin HourMin2)
+        {
+            return !(HourMin1 < HourMin2);
+        }
+
+        #endregion
+
+        #endregion
+
+        #region IComparable<HourMin> Members
+
+        #region CompareTo(Object)
+
+        /// <summary>
+        /// Compares two instances of this object.
+        /// </summary>
+        /// <param name="Object">An object to compare with.</param>
+        public Int32 CompareTo(Object Object)
+        {
+
+            if (Object == null)
+                throw new ArgumentNullException("The given object must not be null!");
+
+            // Check if the given object is an HourMin.
+            if (!(Object is HourMin))
+                throw new ArgumentNullException("The given object is not a HourMin!");
+
+            return CompareTo((HourMin) Object);
+
+        }
+
+        #endregion
+
+        #region CompareTo(HourMin)
+
+        /// <summary>
+        /// Compares two instances of this object.
+        /// </summary>
+        /// <param name="HourMin">An object to compare with.</param>
+        public Int32 CompareTo(HourMin HourMin)
+        {
+
+            if ((Object) HourMin == null)
+                throw new ArgumentNullException("The given HourMin must not be null!");
+
+            // Compare CountryIds
+            var _Result = Hour.CompareTo(HourMin.Hour);
+
+            // If equal: Compare charging operator identifications
+            if (_Result == 0)
+                _Result = Minute.CompareTo(HourMin.Minute);
+
+            return _Result;
+
+        }
+
+        #endregion
+
+        #endregion
+
+        #region IEquatable<HourMin> Members
+
+        #region Equals(Object)
+
+        /// <summary>
+        /// Compares two instances of this object.
+        /// </summary>
+        /// <param name="Object">An object to compare with.</param>
+        /// <returns>true|false</returns>
+        public override Boolean Equals(Object Object)
+        {
+
+            if (Object == null)
+                return false;
+
+            // Check if the given object is an HourMin.
+            if (!(Object is HourMin))
+                return false;
+
+            return this.Equals((HourMin) Object);
+
+        }
+
+        #endregion
+
+        #region Equals(HourMin)
+
+        /// <summary>
+        /// Compares two HourMins for equality.
+        /// </summary>
+        /// <param name="HourMin">A HourMin to compare with.</param>
+        /// <returns>True if both match; False otherwise.</returns>
+        public Boolean Equals(HourMin HourMin)
+        {
+
+            if ((Object) HourMin == null)
+                return false;
+
+            return Hour.  Equals(HourMin.Hour) &&
+                   Minute.Equals(HourMin.Minute);
+
+        }
+
+        #endregion
+
+        #endregion
+
+        #region GetHashCode()
+
+        /// <summary>
+        /// Return the HashCode of this object.
+        /// </summary>
+        /// <returns>The HashCode of this object.</returns>
+        public override Int32 GetHashCode()
+        {
+            unchecked
+            {
+                return Hour.GetHashCode() * 23 ^ Minute.GetHashCode();
+            }
+        }
+
+        #endregion
+
+        #region (override) ToString()
+
+        /// <summary>
+        /// Return a text representation of this object.
+        /// </summary>
+        public override String ToString()
+            => String.Concat(Hour.ToString("D2"), ":", Minute.ToString("D2"));
+
+        #endregion
+
+    }
+
+}
---
+++ Styx/Illias/Time/OpeningTimes.cs
@@ -0,0 +1,524 @@
+﻿/*
+ * Copyright (c) 2010-2018 Achim 'ahzf' Friedland <achim.friedland@graphdefined.com>
+ * This file is part of Illias <http://www.github.com/Vanaheimr/Illias>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#region Usings
+
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+#endregion
+
+namespace org.GraphDefined.Vanaheimr.Illias
+{
+
+    /// <summary>
+    /// Opening times.
+    /// </summary>
+    public class OpeningTimes : IEquatable<OpeningTimes>
+    {
+
+        #region Properties
+
+        #region RegularOpenings
+
+        private readonly RegularHours[] _RegularOpenings;
+
+        /// <summary>
+        /// The regular openings.
+        /// </summary>
+        public IEnumerable<RegularHours> RegularOpenings
+        {
+            get
+            {
+                return _RegularOpenings.Where(rh => !(rh.DayOfWeek == DayOfWeek.Sunday &&
+                                                    rh.PeriodBegin.Hour == 0 && rh.PeriodBegin.Minute == 0 &&
+                                                    rh.PeriodEnd.  Hour == 0 && rh.PeriodEnd.  Minute == 0));
+            }
+        }
+
+        #endregion
+
+        private readonly List<ExceptionalPeriod> _ExceptionalOpenings;
+        private readonly List<ExceptionalPeriod> _ExceptionalClosings;
+
+        #region IsOpen24Hours
+
+        private readonly Boolean _IsOpen24Hours;
+
+        /// <summary>
+        /// 24/7 open...
+        /// </summary>
+        public Boolean IsOpen24Hours
+        {
+            get
+            {
+                return _IsOpen24Hours;
+            }
+        }
+
+        #endregion
+
+        #region FreeText
+
+        private String _FreeText;
+
+        /// <summary>
+        /// An additoonal free text.
+        /// </summary>
+        public String FreeText
+        {
+
+            get
+            {
+                return _FreeText;
+            }
+
+            set
+            {
+                _FreeText = value;
+            }
+
+        }
+
+        #endregion
+
+        #endregion
+
+        #region Constructor(s)
+
+        #region OpeningTime(IsOpen24Hours, FreeText = "")
+
+        private OpeningTimes(Boolean  IsOpen24Hours,
+                             String   FreeText = "")
+        {
+
+            this._RegularOpenings      = new RegularHours[7];
+            this._ExceptionalOpenings  = new List<ExceptionalPeriod>();
+            this._ExceptionalClosings  = new List<ExceptionalPeriod>();
+            this._FreeText             = FreeText;
+            this._IsOpen24Hours        = IsOpen24Hours;
+
+        }
+
+        #endregion
+
+        #region OpeningTime(FromWeekday, ToWeekday, Text = "")
+
+        public OpeningTimes(DayOfWeek  FromWeekday,
+                            DayOfWeek  ToWeekday,
+                            String     FreeText = "")
+
+            : this(false, FreeText)
+
+        {
+
+            SetRegularOpening(FromWeekday, ToWeekday, new HourMin(0, 0), new HourMin(0, 0));
+
+        }
+
+        #endregion
+
+        #region OpeningTime(FromWeekday, ToWeekday, Begin, End, Text = "")
+
+        public OpeningTimes(DayOfWeek  FromWeekday,
+                            DayOfWeek  ToWeekday,
+                            HourMin    Begin,
+                            HourMin    End,
+                            String     FreeText = "")
+
+            : this(false, FreeText)
+
+        {
+
+            SetRegularOpening(FromWeekday, ToWeekday, Begin, End);
+
+        }
+
+        #endregion
+
+        #endregion
+
+
+        public OpeningTimes SetRegularOpening(DayOfWeek  Weekday,
+                                              HourMin    Begin,
+                                              HourMin    End)
+        {
+
+            _RegularOpenings[(int) Weekday] = new RegularHours(Weekday, Begin, End);
+
+            return this;
+
+        }
+
+        public OpeningTimes SetRegularOpening(DayOfWeek  FromWeekday,
+                                              DayOfWeek  ToWeekday,
+                                              HourMin    Begin,
+                                              HourMin    End)
+        {
+
+            var _FromWeekday = (int) FromWeekday;
+            var _ToWeekday   = (int) ToWeekday;
+
+            if (_ToWeekday < _FromWeekday)
+                _ToWeekday += 7;
+
+            for (var weekday = _FromWeekday; weekday <= _ToWeekday; weekday++)
+                _RegularOpenings[weekday % 7] = new RegularHours((DayOfWeek) (weekday % 7), Begin, End);
+
+            return this;
+
+        }
+
+        public OpeningTimes AddExceptionalOpening(DateTime Start, DateTime End)
+        {
+
+            _ExceptionalOpenings.Add(new ExceptionalPeriod(Start, End));
+
+            return this;
+
+        }
+
+        public OpeningTimes AddExceptionalClosing(DateTime Start, DateTime End)
+        {
+
+            _ExceptionalClosings.Add(new ExceptionalPeriod(Start, End));
+
+            return this;
+
+        }
+
+
+        public static OpeningTimes FromFreeText(String   Text,
+                                                Boolean  IsOpen24Hours = true)
+        {
+
+            return new OpeningTimes(IsOpen24Hours, Text);
+
+        }
+
+
+        public static OpeningTimes Parse(String Text)
+        {
+
+            OpeningTimes _OpeningTimes = null;
+
+            if (TryParse(Text, out _OpeningTimes))
+                return _OpeningTimes;
+
+            return null;
+
+        }
+
+        public static Boolean TryParse(String Text, out OpeningTimes OpeningTimes)
+        {
+
+            OpeningTimes = null;
+
+            var match = Regex.Match(Text, "([a-zA-Z]+) - ([a-zA-Z]+) (([0-9]{2}:[0-9]{2})h - ([0-9]{2}:[0-9]{2})h|open|closed)");
+            if (!match.Success)
+                return false;
+
+            #region Parse weekdays
+
+            DayOfWeek FromWeekday;
+
+            switch (match.Groups[1].Value.ToLower())
+            {
+
+                case "mo":
+                case "mon":
+                case "monday":
+                    FromWeekday = DayOfWeek.Monday;
+                    break;
+
+                case "tu":
+                case "di":
+                case "tue":
+                case "tuesday":
+                    FromWeekday = DayOfWeek.Tuesday;
+                    break;
+
+                case "we":
+                case "mi":
+                case "wed":
+                case "wednesday":
+                    FromWeekday = DayOfWeek.Wednesday;
+                    break;
+
+                case "th":
+                case "do":
+                case "thu":
+                case "thursday":
+                    FromWeekday = DayOfWeek.Thursday;
+                    break;
+
+                case "fr":
+                case "fri":
+                case "friday":
+                    FromWeekday = DayOfWeek.Friday;
+                    break;
+
+                case "sa":
+                case "sat":
+                case "saturday":
+                    FromWeekday = DayOfWeek.Saturday;
+                    break;
+
+                case "su":
+                case "so":
+                case "sun":
+                case "sunday":
+                    FromWeekday = DayOfWeek.Sunday;
+                    break;
+
+                default:
+                    return false;
+
+            }
+
+            DayOfWeek ToWeekday;
+
+            switch (match.Groups[2].Value.ToLower())
+            {
+
+                case "mo":
+                case "mon":
+                case "monday":
+                    ToWeekday = DayOfWeek.Monday;
+                    break;
+
+                case "tu":
+                case "di":
+                case "tue":
+                case "tuesday":
+                    ToWeekday = DayOfWeek.Tuesday;
+                    break;
+
+                case "we":
+                case "mi":
+                case "wed":
+                case "wednesday":
+                    ToWeekday = DayOfWeek.Wednesday;
+                    break;
+
+                case "th":
+                case "do":
+                case "thu":
+                case "thursday":
+                    ToWeekday = DayOfWeek.Thursday;
+                    break;
+
+                case "fr":
+                case "fri":
+                case "friday":
+                    ToWeekday = DayOfWeek.Friday;
+                    break;
+
+                case "sa":
+                case "sat":
+                case "saturday":
+                    ToWeekday = DayOfWeek.Saturday;
+                    break;
+
+                case "su":
+                case "so":
+                case "sun":
+                case "sunday":
+                    ToWeekday = DayOfWeek.Sunday;
+                    break;
+
+                default:
+                    return false;
+
+            }
+
+            #endregion
+
+            #region Parse hours...
+
+            HourMin Begin;
+            HourMin End;
+
+            if (HourMin.TryParse(match.Groups[4].Value, out Begin) &&
+                HourMin.TryParse(match.Groups[5].Value, out End))
+            {
+                OpeningTimes = new OpeningTimes(FromWeekday, ToWeekday, Begin, End);
+                return true;
+            }
+
+            #endregion
+
+            #region ...or parse "open"
+
+            else if (match.Groups[3].Value == "open")
+            {
+                OpeningTimes = new OpeningTimes(FromWeekday, ToWeekday);
+                return true;
+            }
+
+            #endregion
+
+            #region ...or parse "closed"
+
+            else if (match.Groups[3].Value == "closed")
+            {
+                OpeningTimes = new OpeningTimes(false, Text);
+                return true;
+            }
+
+            #endregion
+
+            return false;
+
+        }
+
+
+        #region (static) Open24Hours
+
+        /// <summary>
+        /// Is open for 24 hours a day (7 days a week).
+        /// </summary>
+        public static OpeningTimes Open24Hours
+
+            => new OpeningTimes(IsOpen24Hours: true);
+
+        #endregion
+
+
+        #region Operator overloading
+
+        #region Operator == (OpeningTime1, OpeningTime2)
+
+        /// <summary>
+        /// Compares two opening times for equality.
+        /// </summary>
+        /// <param name="OpeningTime1">An opening time.</param>
+        /// <param name="OpeningTime2">Another opening time.</param>
+        /// <returns>True if both match; False otherwise.</returns>
+        public static Boolean operator == (OpeningTimes OpeningTime1, OpeningTimes OpeningTime2)
+        {
+
+            // If both are null, or both are same instance, return true.
+            if (Object.ReferenceEquals(OpeningTime1, OpeningTime2))
+                return true;
+
+            // If one is null, but not both, return false.
+            if (((Object) OpeningTime1 == null) || ((Object) OpeningTime2 == null))
+                return false;
+
+            return OpeningTime1.Equals(OpeningTime2);
+
+        }
+
+        #endregion
+
+        #region Operator != (OpeningTime1, OpeningTime2)
+
+        /// <summary>
+        /// Compares two opening times for inequality.
+        /// </summary>
+        /// <param name="OpeningTime1">An opening time.</param>
+        /// <param name="OpeningTime2">Another opening time.</param>
+        /// <returns>False if both match; True otherwise.</returns>
+        public static Boolean operator != (OpeningTimes OpeningTime1, OpeningTimes OpeningTime2)
+        {
+            return !(OpeningTime1 == OpeningTime2);
+        }
+
+        #endregion
+
+        #endregion
+
+        #region IEquatable<OpeningTime> Members
+
+        #region Equals(Object)
+
+        /// <summary>
+        /// Compares two instances of this object.
+        /// </summary>
+        /// <param name="Object">An object to compare with.</param>
+        /// <returns>true|false</returns>
+        public override Boolean Equals(Object Object)
+        {
+
+            if (Object == null)
+                return false;
+
+            // Check if the given object is an OpeningTime.
+            var OpenTime = Object as OpeningTimes;
+            if ((Object) OpenTime == null)
+                return false;
+
+            return this.Equals(OpenTime);
+
+        }
+
+        #endregion
+
+        #region Equals(OpeningTimes)
+
+        /// <summary>
+        /// Compares two opening times for equality.
+        /// </summary>
+        /// <param name="OpeningTimes">An opening time to compare with.</param>
+        /// <returns>True if both match; False otherwise.</returns>
+        public Boolean Equals(OpeningTimes OpeningTimes)
+        {
+
+            if ((Object) OpeningTimes == null)
+                return false;
+
+            if (IsOpen24Hours && OpeningTimes.IsOpen24Hours)
+                return true;
+
+            return FreeText.Equals(OpeningTimes.FreeText);
+
+        }
+
+        #endregion
+
+        #endregion
+
+        #region GetHashCode()
+
+        /// <summary>
+        /// Get the hashcode of this object.
+        /// </summary>
+        public override Int32 GetHashCode()
+        {
+            return FreeText.GetHashCode();
+        }
+
+        #endregion
+
+        #region (override) ToString()
+
+        /// <summary>
+        /// Return a text representation of this object.
+        /// </summary>
+        public override String ToString()
+        {
+            return IsOpen24Hours ? "24 hours" : FreeText;
+        }
+
+        #endregion
+
+    }
+
+}
