7257c0ed4fc38c120c6ecfb9ffca7bff517f0021
Achim Friedland <achim.friedland@graphdefined.com>
2021-02-04 15:48:43 +0100

    Happy new year (2)

--- StyxTests/Properties/AssemblyInfo.cs
+++ StyxTests/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("GraphDefined GmbH")]
 [assembly: AssemblyProduct("Styx Unit Tests")]
-[assembly: AssemblyCopyright("Copyright © 2010-2020 GraphDefined GmbH")]
+[assembly: AssemblyCopyright("Copyright © 2010-2021 GraphDefined GmbH")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
