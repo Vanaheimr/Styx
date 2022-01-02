cb31c5dcff538a6b9b8c08aa9b6c864bb76a51f8
Achim Friedland <achim.friedland@graphdefined.com>
2022-01-02 16:12:01 +0100

    Happy new year!

--- Styx/Illias/ExtensionMethods/JSONExtentions.cs
+++ Styx/Illias/ExtensionMethods/JSONExtentions.cs
@@ -1,5 +1,5 @@
 ﻿/*
- * Copyright (c) 2010-2021, Achim Friedland <achim.friedland@graphdefined.com>
+ * Copyright (c) 2010-2022, Achim Friedland <achim.friedland@graphdefined.com>
  * This file is part of Vanaheimr Hermod <https://www.github.com/Vanaheimr/Hermod>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
