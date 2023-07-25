3d187f253a8c2eef56a17b00cd95726f43adf99c
Achim Friedland <achim.friedland@graphdefined.com>
2023-07-26 00:43:14 +0200

    Improved resources monitor

--- Styx/Illias/System/ResourcesMonitor.cs
+++ Styx/Illias/System/ResourcesMonitor.cs
@@ -25,12 +25,18 @@ using System.Runtime.InteropServices;
 namespace org.GraphDefined.Vanaheimr.Illias
 {
 
+    public delegate Task RAMUsageMonitorHandler        (ResourcesMonitor  Sender,
+                                                        DateTime          Timestamp,
+                                                        UInt64            RAMUsage,
+                                                        UInt64            RAMUsageThreshold);
 
-    public delegate void MemoryMonitorHandler   (ResourcesMonitor  Sender,
-                                                 MemoryMetrics     MemoryMetrics);
+    public delegate Task FreeSystemMemoryMonitorHandler(ResourcesMonitor  Sender,
+                                                        DateTime          Timestamp,
+                                                        MemoryMetrics     MemoryMetrics);
 
-    public delegate void DiskSpaceMonitorHandler(ResourcesMonitor  Sender,
-                                                 Double            FreeSpacePercentage);
+    public delegate Task LowDiskSpaceMonitorHandler    (ResourcesMonitor  Sender,
+                                                        DateTime          Timestamp,
+                                                        Double            FreeDiscSpacePercentage);
 
     public class MemoryMetrics
     {
@@ -89,103 +95,236 @@ namespace org.GraphDefined.Vanaheimr.Illias
 
         #region Data
 
-        private          Timer?                              timer;
-        private          TimeSpan                            interval;
-        private readonly Double                              FreeMemoryThreshold;
-        private readonly IEnumerable<Tuple<String, Double>>  PathAndFreeSpaceThresholds;
+        private Timer? timer;
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// The checking interval.
+        /// </summary>
+        public TimeSpan                            CheckInterval                 { get; set; }
+
+        /// <summary>
+        /// The current process identification.
+        /// </summary>
+        public Int32                               ProcessId                     { get; }
+
+        /// <summary>
+        /// The current process.
+        /// </summary>
+        public Process                             Process                       { get; }
+
+
+        /// <summary>
+        /// The threshold in MBytes of RAM used before the OnHighRAMUsage event will be triggered.
+        /// </summary>
+        public UInt64                              HighRAMUsageThreshold         { get; }
+
+        /// <summary>
+        /// The threshold in % of free RAM left before the OnLowSystemMemory event will be triggered.
+        /// </summary>
+        public Double                              FreeSystemMemoryThreshold     { get; }
+
+        /// <summary>
+        /// The threshold in % of free hard disc space left before the OnLowDiskSpace event will be triggered.
+        /// </summary>
+        public IEnumerable<Tuple<String, Double>>  PathAndFreeDiscSpaceThresholds    { get; }
+
+
+        /// <summary>
+        /// The latest current RAM usage.
+        /// </summary>
+        public UInt64                              CurrentRAMUsage               { get; private set; }
+
+        /// <summary>
+        /// The latest free system memory metrics.
+        /// </summary>
+        public MemoryMetrics?                      FreeSystemMemoryMetrics       { get; private set; }
+
+        /// <summary>
+        /// The latest free disc space percentage.
+        /// </summary>
+        public Double                              FreeDiscSpacePercentage       { get; private set; }
 
         #endregion
 
         #region Events
 
-        public event MemoryMonitorHandler?     LowMemory;
-        public event DiskSpaceMonitorHandler?  LowDiskSpace;
+        /// <summary>
+        /// An event called whenever the current RAM usage has changed.
+        /// </summary>
+        public event RAMUsageMonitorHandler?          OnCurrentRAMUsage;
+
+        /// <summary>
+        /// An event called whenever the current RAM usage is above its threshold.
+        /// </summary>
+        public event RAMUsageMonitorHandler?          OnHighRAMUsage;
+
+        /// <summary>
+        /// An event called whenever the current free system memory is below its threshold.
+        /// </summary>
+        public event FreeSystemMemoryMonitorHandler?  OnLowSystemMemory;
+
+        /// <summary>
+        /// An event called whenever the current free disc space is below its threshold.
+        /// </summary>
+        public event LowDiskSpaceMonitorHandler?      OnLowDiskSpace;
 
         #endregion
 
         #region Constructor(s)
 
-        // Path.GetPathRoot(AppDomain.CurrentDomain.BaseDirectory)
-        public ResourcesMonitor(Double                              FreeMemoryThreshold,
+        /// <summary>
+        /// Create a new resources monitor.
+        /// </summary>
+        /// <param name="HighRAMUsageThreshold">An threshold in MBytes of RAM used before the OnHighRAMUsage event will be triggered.</param>
+        /// <param name="FreeSystemMemoryThreshold">An threshold in % of free RAM left before the OnLowSystemMemory event will be triggered.</param>
+        /// <param name="PathAndFreeSpaceThresholds">An threshold in % of free hard disc space left before the OnLowDiskSpace event will be triggered.</param>
+        /// <param name="CheckInterval">An optional checking interval.</param>
+        public ResourcesMonitor(UInt64                              HighRAMUsageThreshold,
+                                Double                              FreeSystemMemoryThreshold,
                                 IEnumerable<Tuple<String, Double>>  PathAndFreeSpaceThresholds,
-                                TimeSpan?                           Interval   = null)
+                                TimeSpan?                           CheckInterval   = null)
         {
 
-            this.FreeMemoryThreshold         = FreeMemoryThreshold;
-            this.PathAndFreeSpaceThresholds  = PathAndFreeSpaceThresholds.Distinct();
+            this.CheckInterval               = CheckInterval ?? TimeSpan.FromMinutes(1);
 
-            if (Interval.HasValue)
-                StartMonitoring(Interval.Value);
+            this.ProcessId                   = Environment.ProcessId;
+            this.Process                     = Process.GetProcessById(ProcessId);
+
+            this.HighRAMUsageThreshold       = HighRAMUsageThreshold;
+            this.FreeSystemMemoryThreshold   = FreeSystemMemoryThreshold;
+            this.PathAndFreeDiscSpaceThresholds  = PathAndFreeSpaceThresholds.Distinct();
+
+            if (CheckInterval.HasValue)
+                StartMonitoring(CheckInterval.Value);
 
         }
 
         #endregion
 
-
-        #region StartMonitoring()
-
-        public void StartMonitoring(TimeSpan Interval)
-        {
-            this.interval  = Interval;
-            this.timer     = new Timer(CheckResources, null, TimeSpan.Zero, Interval);
-        }
-
-        #endregion
-
-        #region StopMonitoring()
-
-        public void StopMonitoring()
-        {
-            this.timer?.Dispose();
-        }
-
-        #endregion
 
         #region (private, timer) CheckResources(State)
 
-        private void CheckResources(Object? State)
+        private async void CheckResources(Object? State)
         {
 
-            // Free memory in percent
-            var memoryMetrics = RuntimeInformation.IsOSPlatform(OSPlatform.OSX) ||
-                                RuntimeInformation.IsOSPlatform(OSPlatform.Linux)
-                                    ? GetUnixMetrics()
-                                    : GetWindowsMetrics();
+            var now = Timestamp.Now;
 
-            if (memoryMetrics is not null &&
-                memoryMetrics.FreePercentage < FreeMemoryThreshold)
-            {
-                LowMemory?.Invoke(this, memoryMetrics);
-            }
+            #region RAM usage of this process (MBytes)
 
+            CurrentRAMUsage = (UInt64) Process.WorkingSet64 / (1024 * 1024);
 
-            // Free space in percent
-            foreach (var pathAndFreeSpaceThreshold in PathAndFreeSpaceThresholds)
+            // High RAM usage
+            if (CurrentRAMUsage > HighRAMUsageThreshold)
             {
 
-                var driveInfo            = new DriveInfo(pathAndFreeSpaceThreshold.Item1);
-                var freeSpacePercentage  = (Double) driveInfo.AvailableFreeSpace / driveInfo.TotalSize * 100;
+                var onHighRAMUsage = OnHighRAMUsage?.GetInvocationList()?.Cast<RAMUsageMonitorHandler>()
+                                         ?? Array.Empty<RAMUsageMonitorHandler>();
 
-                if (freeSpacePercentage < pathAndFreeSpaceThreshold.Item2)
-                    LowDiskSpace?.Invoke(this, freeSpacePercentage);
+                if (onHighRAMUsage.Any())
+                    await Task.WhenAll(onHighRAMUsage.
+                                       Select(async ramUsageMonitorHandler => {
+                                           await ramUsageMonitorHandler(this,
+                                                                        now,
+                                                                        CurrentRAMUsage,
+                                                                        HighRAMUsageThreshold);
+                                       })).
+                                       ConfigureAwait(false);
 
             }
+
+            // Current RAM usage
+            var onCurrentRAMUsage = OnCurrentRAMUsage?.GetInvocationList()?.Cast<RAMUsageMonitorHandler>()
+                                        ?? Array.Empty<RAMUsageMonitorHandler>();
+
+            if (onCurrentRAMUsage.Any())
+                await Task.WhenAll(onCurrentRAMUsage.
+                                   Select(async ramUsageMonitorHandler => {
+                                       await ramUsageMonitorHandler(this,
+                                                                    now,
+                                                                    CurrentRAMUsage,
+                                                                    HighRAMUsageThreshold);
+                                   })).
+                                   ConfigureAwait(false);
+
+            #endregion
+
+            #region Free system memory        (%)
+
+            FreeSystemMemoryMetrics = RuntimeInformation.IsOSPlatform(OSPlatform.OSX) ||
+                                      RuntimeInformation.IsOSPlatform(OSPlatform.Linux)
+                                          ? GetMemoryMetricsOnUnix()
+                                          : GetMemoryMetricsOnWindows();
+
+            if (FreeSystemMemoryMetrics is not null &&
+                FreeSystemMemoryMetrics.FreePercentage < FreeSystemMemoryThreshold)
+            {
+
+                var onLowSystemMemory = OnLowSystemMemory?.GetInvocationList()?.Cast<FreeSystemMemoryMonitorHandler>()
+                                             ?? Array.Empty<FreeSystemMemoryMonitorHandler>();
+
+                if (onLowSystemMemory.Any())
+                    await Task.WhenAll(onLowSystemMemory.
+                                       Select(async freeSystemMemoryMonitorHandler => {
+                                           await freeSystemMemoryMonitorHandler(this,
+                                                                                now,
+                                                                                FreeSystemMemoryMetrics);
+                                       })).
+                                       ConfigureAwait(false);
+
+            }
+
+            #endregion
+
+            #region Free disc space           (%)
+
+            foreach (var pathAndFreeDiscSpaceThreshold in PathAndFreeDiscSpaceThresholds)
+            {
+
+                var driveInfo            = new DriveInfo(pathAndFreeDiscSpaceThreshold.Item1);
+                FreeDiscSpacePercentage  = (Double) driveInfo.AvailableFreeSpace / driveInfo.TotalSize * 100;
+
+                if (FreeDiscSpacePercentage < pathAndFreeDiscSpaceThreshold.Item2)
+                {
+
+                    var onLowDiskSpace = OnLowDiskSpace?.GetInvocationList()?.Cast<LowDiskSpaceMonitorHandler>()
+                                             ?? Array.Empty<LowDiskSpaceMonitorHandler>();
+
+                    if (onLowDiskSpace.Any())
+                        await Task.WhenAll(onLowDiskSpace.
+                                           Select(async lowDiskSpaceMonitorHandler => {
+                                               await lowDiskSpaceMonitorHandler(this,
+                                                                                now,
+                                                                                FreeDiscSpacePercentage);
+                                           })).
+                                           ConfigureAwait(false);
+
+                }
+
+            }
+
+            #endregion
 
         }
 
         #endregion
 
+        #region (private static) GetMemoryMetricsOnWindows()
 
-        #region (private) GetWindowsMetrics()
-
-        private MemoryMetrics? GetWindowsMetrics()
+        /// <summary>
+        /// Get memory metrics on Windows.
+        /// </summary>
+        private static MemoryMetrics? GetMemoryMetricsOnWindows()
         {
 
             var processInfo = new ProcessStartInfo {
-                FileName                = "wmic",
-                Arguments               = "OS get FreePhysicalMemory,TotalVisibleMemorySize /Value",
-                RedirectStandardOutput  = true
-            };
+                                  FileName                = "wmic",
+                                  Arguments               = "OS get FreePhysicalMemory,TotalVisibleMemorySize /Value",
+                                  RedirectStandardOutput  = true
+                              };
 
             var output = "";
 
@@ -215,16 +354,19 @@ namespace org.GraphDefined.Vanaheimr.Illias
 
         #endregion
 
-        #region (private) GetUnixMetrics()
+        #region (private static) GetMemoryMetricsOnUnix()
 
-        private MemoryMetrics? GetUnixMetrics()
+        /// <summary>
+        /// Get memory metrics on UNIX.
+        /// </summary>
+        private static MemoryMetrics? GetMemoryMetricsOnUnix()
         {
 
             var processInfo = new ProcessStartInfo("free -m") {
-                FileName                = "/bin/bash",
-                Arguments               = "-c \"free -m\"",
-                RedirectStandardOutput  = true
-            };
+                                  FileName                = "/bin/bash",
+                                  Arguments               = "-c \"free -m\"",
+                                  RedirectStandardOutput  = true
+                              };
 
             var output = "";
 
@@ -250,6 +392,43 @@ namespace org.GraphDefined.Vanaheimr.Illias
 
             return null;
 
+        }
+
+        #endregion
+
+
+        #region StartMonitoring()
+
+        /// <summary>
+        /// Start monitoring.
+        /// </summary>
+        /// <param name="Interval">The check service interval.</param>
+        public void StartMonitoring(TimeSpan Interval)
+        {
+            if (timer is null)
+            {
+
+                CheckInterval  = Interval;
+
+                timer          = new Timer(CheckResources,
+                                           null,
+                                           CheckInterval,
+                                           CheckInterval);
+
+            }
+        }
+
+        #endregion
+
+        #region StopMonitoring()
+
+        public void StopMonitoring()
+        {
+            if (timer is not null)
+            {
+                timer?.Dispose();
+                timer = null;
+            }
         }
 
         #endregion
