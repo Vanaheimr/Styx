42f4c764738ff359a5d5cdcef0bcdb208a2458ca
Achim Friedland <achim.friedland@graphdefined.com>
2025-07-09 00:06:21 +0200

    Adden some logfile writers via channels

---
+++ Styx/Illias/Logging/LogFileWriter.cs
@@ -0,0 +1,102 @@
+﻿/*
+ * Copyright (c) 2010-2025 GraphDefined GmbH <achim.friedland@graphdefined.com>
+ * This file is part of Illias <https://www.github.com/Vanaheimr/Illias>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#region Usings
+
+using System.Text;
+using System.Threading.Channels;
+
+#endregion
+
+namespace org.GraphDefined.Vanaheimr.Illias.Logging
+{
+
+    public sealed class LogFileWriter : IAsyncDisposable
+    {
+
+        private readonly Channel<(String FileName, String Line)>  channel;
+        private readonly CancellationTokenSource                  cts = new();
+        private readonly Task                                     backgroundTask;
+
+        public LogFileWriter(Int32 capacity = 1000)
+        {
+
+            this.channel         = Channel.CreateBounded<(String, String)>(
+                                       new BoundedChannelOptions(capacity) {
+                                           FullMode      = BoundedChannelFullMode.Wait,
+                                           SingleReader  = true,
+                                           SingleWriter  = false
+                                       }
+                                   );
+
+            this.backgroundTask  = Task.Run(WriteLoopAsync);
+
+        }
+
+        public ValueTask EnqueueAsync(String             FileName,
+                                      String             Line,
+                                      CancellationToken  cancellationToken   = default)
+
+            => channel.Writer.WriteAsync(
+                   (FileName, Line),
+                   cancellationToken
+               );
+
+
+        private async Task WriteLoopAsync()
+        {
+            try
+            {
+                await foreach (var (fileName, line) in channel.Reader.ReadAllAsync(cts.Token))
+                {
+                    try
+                    {
+
+                        await File.AppendAllTextAsync(
+                                  fileName,
+                                  line + Environment.NewLine,
+                                  Encoding.UTF8,
+                                  cts.Token
+                              );
+
+                    }
+                    catch (Exception ex)
+                    {
+                        DebugX.Log($"[ERROR] Could not write to file {fileName}: {ex.Message}");
+                    }
+                }
+            }
+            catch (OperationCanceledException)
+            {
+                // Shutdown
+            }
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+
+            channel.Writer.Complete();
+            cts.Cancel();
+
+            await backgroundTask.ConfigureAwait(false);
+            cts.Dispose();
+
+        }
+
+    }
+
+}
--- Styx/Illias/Logging/Warning.cs
+++ Styx/Illias/Logging/Warning.cs
@@ -1,12 +1,12 @@
 ﻿/*
- * Copyright (c) 2014 Achim Friedland <achim.friedland@graphdefined.com>
- * This file is part of eMI3 WWCP <https://www.github.com/eMI3/WWCP-Bindings>
+ * Copyright (c) 2010-2025 GraphDefined GmbH <achim.friedland@graphdefined.com>
+ * This file is part of Illias <https://www.github.com/Vanaheimr/Illias>
  *
- * Licensed under the Affero GPL license, Version 3.0 (the "License");
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.gnu.org/licenses/agpl.html
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
